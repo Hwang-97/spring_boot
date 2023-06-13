a65fbcfa9f8b0832e79b40e95d1e0f53c0e96899
Hwang-97 <qqq4472@gmail.com>
2023-06-13 14:49:36 +0900

    template completion

---
+++ frontend/src/components/Template.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Template.vue
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Template'
+}
+</script>
+
+<style scoped>
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
-import App from './App.vue'
+import App from '@/App.vue'
+import router from "@/router";
+import store from "@/store";
 
-const vm = new Vue({
+new Vue({
+    router,
+    store,
     render: (h) => h(App),
 }).$mount('#app')
